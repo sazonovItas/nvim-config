72fea98c55b3daee8514e6b01d2bd30c1aa2dc6a
sazonovItas <itassudo@gmail.com>
2023-08-16 16:20:28 +0300

    Added keymaps for markdown-plugin

--- lua/itas/core/keymaps.lua
+++ lua/itas/core/keymaps.lua
@@ -22,15 +22,15 @@ keymap.set("n", "<leader>+", "<C-a>") -- increment
 keymap.set("n", "<leader>-", "<C-x>") -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
-keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v")        -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s")        -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=")        -- make split windows equal width & height
+keymap.set("n", "<leader>sx", ":close<CR>")    -- close current split window
 
-keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>to", ":tabnew<CR>")   -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
-keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
-keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+keymap.set("n", "<leader>tn", ":tabn<CR>")     --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>")     --  go to previous tab
 
 -----------------------
 -- Plugin Keybinds
@@ -45,3 +45,8 @@ keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
 keymap.set("n", "<leader>fg", ":Telescope live_grep<CR>")
 keymap.set("n", "<leader>fb", ":Telescope buffers<CR>")
 keymap.set("n", "<leader>fh", ":Telescope help_tags<CR>")
+
+-- markdown
+keymap.set("n", "<leader>mdp", ":MarkdownPreview<CR>")
+keymap.set("n", "<leader>mds", ":MarkdownPreviewStop<CR>")
+keymap.set("n", "<leader>mdt", ":MarkdownPreviewToggle<CR>")
